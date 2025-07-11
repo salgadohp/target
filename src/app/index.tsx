94a4bdee46700bb04e64024fa10da5950dc92112
Pedro Salgado <salgadopedroh@gmail.com>
2025-07-10 23:22:23 -0300

    passing parameters through expo router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
-
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
     return (
-        <View >
+        <View style={{flex: 1, justifyContent: "center"}}>
             <Text>Expo Router</Text>
+
+            <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+
+            <Button title="Transação" onPress={() => router.navigate("/transaction/767574")} />
         </View>
     )
 }
