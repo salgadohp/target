f893ea393891bc57722cb13cc82634f6e7fb299b
Pedro Salgado <salgadopedroh@gmail.com>
2025-07-10 23:59:46 -0300

    add inter font family

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,22 @@
 import { colors } from '@/theme/color'
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+  useFonts,
+} from '@expo-google-fonts/inter'
 import { Stack } from 'expo-router'
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
   return (
     <Stack
       screenOptions={{
