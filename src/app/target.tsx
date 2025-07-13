f7d14e7029601c3534fd7e7fc2e6ddd7b7e314d4
Pedro Salgado <salgadopedroh@gmail.com>
2025-07-13 07:47:06 -0300

    add Input component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,6 +1,7 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
-import { router } from "expo-router";
-import { Button, View } from "react-native";
+import { View } from "react-native";
 
 export default function Target(){
     return (
@@ -9,7 +10,13 @@ export default function Target(){
                 title="Meta"
                 subtitle="Economize para alcançar sua meta financeira"
             />
-            <Button title="Voltar" onPress={() => router.back()} /> 
+            <View style={{marginTop: 32, gap: 24}}>
+                <Input
+                    label="Nova meta"
+                    placeholder="Ex: Viagem para praia"
+                />
+                <Button title="Salvar" />
+            </View>
         </View>
     )
 }
