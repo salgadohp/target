7adc21cc4a98e9857f703789000aaa55f21cd572
Pedro Salgado <salgadopedroh@gmail.com>
2025-07-12 22:26:40 -0300

    add target component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
 
 const summary = {
     total: 'R$ 2.680,00',
@@ -8,10 +9,21 @@ const summary = {
     output: { label: 'Saídas', value: '-R$ 883,65' },
 }
 
+const targets = [
+  {
+    name: 'Comprar uma cadeira ergonômica',
+    percentage: '75%',
+    current: '900,00',
+    target: '1.200,00',
+  },
+]
+
 export default function Index() {
     return (
         <View style={{ flex: 1 }}>
             <HomeHeader data={summary} />
+
+            <Target data={targets[0]} />
         </View>
     )
 }
