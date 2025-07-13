e7d4bde3a30bd4cbd7a3b1925eb09a16d85d2ea2
Pedro Salgado <salgadopedroh@gmail.com>
2025-07-12 23:52:13 -0300

    add PageHeader component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 
 import { Button } from "@/components/Button";
 import { HomeHeader } from "@/components/HomeHeader";
@@ -39,6 +39,7 @@ const targets = [
 export default function Index() {
     return (
         <View style={{ flex: 1 }}>
+            <StatusBar barStyle="light-content" />
             <HomeHeader data={summary} />
 
             <List
