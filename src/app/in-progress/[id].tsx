6284df96f92aaf757902fbc1661f7956f5316067
Pedro Salgado <salgadopedroh@gmail.com>
2025-07-16 23:09:58 -0300

    add new transaction button

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/Button";
 import { List } from "@/components/List";
 import { PageHeader } from "@/components/PageHeader";
 import { Progress } from "@/components/Progress";
 import { Transaction, TransactionProps } from "@/components/Transaction";
 import { TransactionTypes } from "@/utils/TransactionTypes";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
 
 const details = {
@@ -49,6 +50,12 @@ export default function InProgress() {
                 renderItem={({item}) => (
                     <Transaction data={item} onRemove={() => {}} />
                 )}
+                emptyMessage="Nenhuma transação. Toque em nova transação para guardar seu primeiro dinheiro aqui."
+            />
+
+            <Button
+                title="Nova transação"
+                onPress={() => router.navigate(`/transaction/${params.id}`)}
             />
         </View>
     )
